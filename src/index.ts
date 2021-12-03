9320b37184eff15971e7c3a2c0e6fed85ba9937c
Daniel Woodruff <92489496+ClarityCreated@users.noreply.github.com>
2021-12-03 13:19:19 -0800

    Front

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+
+const transferBoxEle = document.getElementById("transfer-box")
+const fromAccEle = document.getElementById("fromAcc")
+const toAccEle = document.getElementById("toAcc")
+const amountEle = document.getElementById("amount")
+
+fromAccEle.addEventListener('click', e=>{
+    
+})
